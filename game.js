76e26a0a5392f812b0d28e964d37c5c3c38f5087
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-02 22:53:36 +0600

    check level answer

--- game.js
+++ game.js
@@ -3,6 +3,29 @@ var userClickedPattern = [];
 
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+var flag=0;
+var level=0;
+
+$(document).keydown(function() {
+    if (flag===0){
+        flag=1;
+        nextSequence();
+        $("h1").text("Level " + level);
+    }
+});
+
+$(".btn").click(function() {
+
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+
+    animatePress(userChosenColour);
+
+    checkAnswer(level-1);
+});
+
 function nextSequence(){
 
     var randmNumber = Math.floor(Math.random() * 4);;
@@ -13,21 +36,10 @@ function nextSequence(){
     $(id).fadeIn(100).fadeOut(100).fadeIn(100);
 
     playSound(randomChosenColour);
+
+    level++;
+    $("h1").text("Level " + level);
 }
-
-$(".btn").click(function() {
-
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-
-    // alert(userChosenColour);
-
-    playSound(userChosenColour);
-
-    animatePress(userChosenColour);
-
-});
-
 
 function playSound(name){
 
@@ -43,6 +55,19 @@ function animatePress(currentColour){
 
     setTimeout(function() { 
         $(btn).removeClass("pressed");
-    }, 100);    
+    }, 1000);    
+
+}
+
+function checkAnswer(currentLevel){
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel])
+    {
+        setTimeout(nextSequence(), 1000);
+        userClickedPattern = [];
+    }  
+    else{
+    }
+}
+function gameOver(){
 
 }
