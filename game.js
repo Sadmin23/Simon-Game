15bfc57995fba20fc6d5b911758f936a09a8483f
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-02 19:27:08 +0600

    random buttons selector

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-alert("Hello");
+var buttonColours = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence(){
+    return Math.floor(Math.random() * 4);
+}
+
+var randmNumber = nextSequence();
+
+var randomChosenColour = buttonColours[randmNumber];
+
+buttonColours.push(randomChosenColour);
