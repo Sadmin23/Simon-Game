c062a69065f54707bcb44458580e0858171b16bf
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-03 19:16:03 +0600

    game over

--- game.js
+++ game.js
@@ -73,11 +73,13 @@ function checkAnswer(currentLevel) {
         }
     }
     else {
-        // var audio = new Audio("sounds/wrong.mp3");
-        // audio.play();        
+        var audio = new Audio("sounds/wrong.mp3");
+        audio.play();
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");            
+        }, 200);
+
         console.log("Wrong");
     }
-}
-function gameOver() {
-
 }
